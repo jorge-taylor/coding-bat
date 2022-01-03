67512fbd9c6c038940a845ba1ff6252b031d85b2
jorge-taylor <jorgetaylor@protonmail.com>
2022-01-03 13:24:09 +0000

    added logic-1

---
+++ python/logic-1/alarm_clock.py
@@ -0,0 +1,10 @@
+def alarm_clock(day, vacation):
+  if vacation:
+    if day == 6 or day == 0:
+      return "off"
+    else:
+      return "10:00"
+  elif not vacation and 1 <= day <= 5:
+    return "7:00"
+  else:
+    return "10:00"
